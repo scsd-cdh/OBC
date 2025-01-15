d7c13384aa0f6fdd6125230aca91780af7165fe1
Brendan Kelly <brewkelly@gmail.com>
2025-01-15 13:44:33 -0500

    This got the device ID exactly once via MSP from MRAM
    WIP. Wanted to capture without breathing too hard on anything
    since last time we got something from the MRAM it only worked once

---
+++ Tools/MSP_SPI_MRAM_demo/eusci_b_spi_ex1_master/Debug/subdir_vars.mk
@@ -0,0 +1,29 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430fr5969.cmd 
+
+C_SRCS += \
+../eusci_b_spi_ex1_master.c 
+
+C_DEPS += \
+./eusci_b_spi_ex1_master.d 
+
+OBJS += \
+./eusci_b_spi_ex1_master.obj 
+
+OBJS__QUOTED += \
+"eusci_b_spi_ex1_master.obj" 
+
+C_DEPS__QUOTED += \
+"eusci_b_spi_ex1_master.d" 
+
+C_SRCS__QUOTED += \
+"../eusci_b_spi_ex1_master.c" 
+
+
