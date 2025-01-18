d455b1606cc16683a9de8e76fa88f6598d964e18
Siri2K <kanagalingham.sriram@gmail.com>
2025-01-18 14:05:34 -0500

    Created GPIO FLag Monitoring Code

---
+++ Tools/GPIO_Monitor_flags/gpio_output/Debug/GPIO/subdir_vars.mk
@@ -0,0 +1,26 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../GPIO/GPIO_Read.c 
+
+C_DEPS += \
+./GPIO/GPIO_Read.d 
+
+OBJS += \
+./GPIO/GPIO_Read.obj 
+
+OBJS__QUOTED += \
+"GPIO\GPIO_Read.obj" 
+
+C_DEPS__QUOTED += \
+"GPIO\GPIO_Read.d" 
+
+C_SRCS__QUOTED += \
+"../GPIO/GPIO_Read.c" 
+
+
