a5456712446ff47c77a6d7d74ee2ea030341abcc
Brendan Kelly <brewkelly@gmail.com>
2025-01-25 19:17:13 -0500

    Made functions more standardised by making spitransfer return the array rather than read_device_id

--- Tools/MSP_SPI_MRAM_demo/eusci_b_spi_ex1_master/MRAM_SPI_Middleware.c
+++ Tools/MSP_SPI_MRAM_demo/eusci_b_spi_ex1_master/MRAM_SPI_Middleware.c
@@ -1,11 +1,24 @@
 #include "driverlib.h"
+#include <stddef.h>
 #include "MRAM_SPI_Middleware.h"
 
-static volatile uint8_t numBytesExpected = 0;
+#define READ_DEVICE_ID_CMD 0x9F  // Command to read device ID (typical for MRAM)
+#define READ_UNIQUE_ID_CMD 0x4C  // Command to read unique ID (typical for MRAM)
+#define Read_Memory_Array 0x03  // Command to Read Memory Array (typical for MRAM)
+#define Write_Memory_Array 0x02  // Command to Write Memory Array (typical for MRAM)
+#define Write_Memory_Enable 0x06  // Command to Write Memory Enable (typical for MRAM)
+#define Write_Memory_Disable 0x04  // Command to Write Memory Disable (typical for MRAM)
+#define RX_BUFFER_SIZE 128
+
+static volatile int8_t numBytesExpected = 0;
+
+static volatile uint8_t* rx_buffer_ptr = NULL;
+
 static volatile uint8_t device_id[4] = {0};
 // FIXME: disgusting 
 static volatile uint8_t haveSeenFirstByte = 0;
 static volatile uint8_t originalNumBytes = 0;
+static volatile uint8_t ISR_index = 0;
 
 void CS_LOW()
 {
@@ -102,22 +115,31 @@ void initSPI()
         EUSCI_B_SPI_RECEIVE_INTERRUPT);
 }
 
-void spiTransfer(uint8_t byte)
+void writeMemoryEn(){
+    CS_LOW();
+    spiTransfer(Write_Memory_Enable, NULL, 0);
+    CS_HIGH();
+}
+
+void spiTransfer(uint8_t cmd, uint8_t* rx_data, uint8_t rx_size)
 {
+    numBytesExpected = rx_size;
+    rx_buffer_ptr = rx_data;
+
     while (!EUSCI_B_SPI_getInterruptStatus(EUSCI_B0_BASE, EUSCI_B_SPI_TRANSMIT_INTERRUPT));
-    EUSCI_B_SPI_transmitData(EUSCI_B0_BASE, byte);
+    EUSCI_B_SPI_transmitData(EUSCI_B0_BASE, cmd);
+    __bis_SR_register(LPM0_bits + GIE); // enable interrupts put in low power mode
+
+    // memcpy(rx_data, RX_buffer, rx_size/* TODO MIN(RX_BUFFER_SIZE, rx_size)*/);
 }
 
 void readUniqueId(uint8_t id_buffer[4])
 {
-    numBytesExpected = 4;
     CS_LOW();
-    spiTransfer(0x9F);
-    __bis_SR_register(LPM0_bits + GIE); // enable interrupts put in low power mode
+    spiTransfer(READ_DEVICE_ID_CMD, id_buffer, 4);
 
     // Once we exit the interrupt we jump back here via __bic_SR_register_on_exit(LPM0_bits);
     CS_HIGH();
-    memcpy(id_buffer, (void const*)device_id, 4);
 }
 
 #if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
@@ -132,26 +154,26 @@ void USCI_B0_ISR (void)
     {
         case USCI_SPI_UCRXIFG:      // UCRXIFG
             if (numBytesExpected <= 0) {
+                ISR_index = 0;
                 __bic_SR_register_on_exit(LPM0_bits);
+                return;
             }
 
-            //USCI_B0 TX buffer ready?
-            while (!EUSCI_B_SPI_getInterruptStatus(EUSCI_B0_BASE,
-                        EUSCI_B_SPI_TRANSMIT_INTERRUPT));
+            // //USCI_B0 TX buffer ready?
+            // while (!EUSCI_B_SPI_getInterruptStatus(EUSCI_B0_BASE,
+            //             EUSCI_B_SPI_TRANSMIT_INTERRUPT));
 
             // NOTE: we need to skip the first byte because it will always be zero
             if (haveSeenFirstByte) {
-                device_id[originalNumBytes - numBytesExpected] = EUSCI_B_SPI_receiveData(EUSCI_B0_BASE);
+                rx_buffer_ptr[ISR_index] = EUSCI_B_SPI_receiveData(EUSCI_B0_BASE);
                 numBytesExpected--;
-            } else {
-                originalNumBytes = numBytesExpected;
+                ISR_index++;
+            }else{
+                haveSeenFirstByte = 1;
             }
-            haveSeenFirstByte = 1;
 
             //Send next value
-            EUSCI_B_SPI_transmitData(EUSCI_B0_BASE,
-                0x00
-                );
+            EUSCI_B_SPI_transmitData(EUSCI_B0_BASE, 0x00);
 
             //Delay between transmissions for slave to process information
             __delay_cycles(40);
--- Tools/MSP_SPI_MRAM_demo/eusci_b_spi_ex1_master/MRAM_SPI_Middleware.h
+++ Tools/MSP_SPI_MRAM_demo/eusci_b_spi_ex1_master/MRAM_SPI_Middleware.h
@@ -1,5 +1,13 @@
+#include <stdint.h>
+
+#ifndef _MRAM_SPI_
+#define _MRAM_SPI_
+
 void CS_LOW();
 void CS_HIGH();
 void initSPI();
-void spiTransfer(uint8_t byte);
+void spi_read(uint8_t cmd, uint8_t * rx_data, uint8_t rx_size);
 void readUniqueId(uint8_t id_buffer[4]);
+void writeMemoryEn();
+
+#endif  //_MRAM_SPI_
