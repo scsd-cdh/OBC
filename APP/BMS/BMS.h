6fe00cc73ef23d8d3e655c7054a2fa4339efcbac
Brendan Kelly <brewkelly@gmail.com>
2025-05-19 17:15:00 -0400

    BMS: Read GPIO flags

--- APP/BMS/BMS.h
+++ APP/BMS/BMS.h
@@ -12,15 +12,15 @@ void initBSP();
 // NOTE: Strangest thing... If I don't assign these, when they get passed into TINYPROTOCOL_RegisterTelemetryChannel
 // The copied variable appears to be incremented by 1... No idea
 enum BMS_TelemetryRequestCmdId {
-  BMS_SYSTEM_STATUS_ID = TINYPROTOCOL_TLM_RESERVED,
-  BMS_HEALTH_CHECK_ID = 1,
-  BMS_FLAG_ID = 2,
-  BMS_CURRENT_DRAW_ID = 3,
-  BMS_CURRENT_CHARGE_ID = 4,
-  BMS_VOLTAGE_BATTERY1_ID = 5,
-  BMS_VOLTAGE_BATTERY2_ID = 6,
-  BMS_VOLTAGE_COMBINED_ID = 7,
-  BMS_HEATERS_CONTROLLER_ID = 8,
+  BMS_SYSTEM_STATUS_ID = 1,
+  BMS_HEALTH_CHECK_ID = 2,
+  BMS_FLAG_ID = 3,
+  BMS_CURRENT_DRAW_ID = 4,
+  BMS_CURRENT_CHARGE_ID = 5,
+  BMS_VOLTAGE_BATTERY1_ID = 6,
+  BMS_VOLTAGE_BATTERY2_ID = 7,
+  BMS_VOLTAGE_COMBINED_ID = 8,
+  BMS_HEATERS_CONTROLLER_ID = 9,
 };
 
 typedef union SystemStatusResp {
@@ -55,7 +55,10 @@ typedef union CombinedVoltageResp {
     uint8_t buffer[CURRENT_VOLTAGE_RESP_LEN];
 } CombinedVoltageResp_t;
 
-typedef uint8_t Flag_t;
+typedef union Flag {
+    uint16_t val : 16;
+    uint8_t buffer[2];
+} Flag_t;
 
 void InitAppComm(void);
 
