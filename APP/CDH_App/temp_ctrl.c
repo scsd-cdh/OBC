f37e2124ef1cacbdd6a0346e918321f8fbc671aa
Brendan Kelly <brewkelly@gmail.com>
2025-11-25 17:55:21 -0500

    CDH: temp ctrl WIP

---
+++ APP/CDH_App/temp_ctrl.c
@@ -0,0 +1,8 @@
+//
+// Created by EXT-BXK09021 on 11/21/2025.
+//
+
+#include "temp_ctrl.h"
+#include <commsdrive.h>
+
+#define BMS_SLAVE_ADDR 0x09
---
+++ APP/CDH_App/temp_ctrl.h
@@ -0,0 +1,8 @@
+//
+// Created by EXT-BXK09021 on 11/21/2025.
+//
+
+#ifndef OBC_TEMP_CTRL_H
+#define OBC_TEMP_CTRL_H
+
+#endif //OBC_TEMP_CTRL_H
