b21aff8e240ca15dc74d6c16e57b2c8d5299b405
alexkar598 <25136265+alexkar598@users.noreply.github.com>
2026-01-26 18:42:55 -0500

    Demo stuff

--- APP/CDH_App/src/main.c
+++ APP/CDH_App/src/main.c
@@ -1,58 +1,87 @@
 #include "main.h"
 
+#include <assert.h>
 #include <stdio.h>
-//#include <ulog/ulog.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/gpio.h>
-#include <zephyr/drivers/led.h>
+#include <zephyr/drivers/i2c.h>
+#include <zephyr/drivers/uart.h>
 
-//#include "logging.h"
-#include "zephyr/drivers/i2c.h"
+static const struct i2c_dt_spec dev_test = I2C_DT_SPEC_GET(DT_NODELABEL(demo_i2c));
+void demo_i2c(void)
+{
+    printk("I2C Demo: Start\n");
 
-// static const struct led_dt_spec dev_led = LED_DT_SPEC_GET(DT_ALIAS(led0));
-static const struct i2c_dt_spec dev_test = I2C_DT_SPEC_GET(DT_NODELABEL(i2c_test));
+    char * msg = "Hello!";
+
+    // Write out message via i2c
+    i2c_write_dt(&dev_test, (uint8_t *)msg, strlen(msg) + 1);
+
+    // Allocate buffer for response
+    uint8_t buf[128];
+
+    // Read the response into the buffer
+    i2c_read_dt(&dev_test, buf, strlen(msg));
+
+    // Check if we got back what we sent
+    printk("I2C Demo: Sent \"%s\", got back \"%s\"\n", msg, (char *)buf);
+    __ASSERT(strcmp(buf, msg) == 0, "I2C demo mismatch");
+
+    printk("I2C Demo: Done\n");
+}
+
+
+
+static const struct device * dev_demo_uart = DEVICE_DT_GET(DT_NODELABEL(demo_serial));
+void demo_uart(void)
+{
+    printk("UART Demo: Start\n");
+
+    char * msg = "Testing.";
+
+    // Send the message character by character
+    for (char * chr = msg; *chr != 0; ++chr)
+    {
+        uart_poll_out(dev_demo_uart, *chr);
+    }
+
+    // Allocate buffer to receive response
+    uint8_t rx_buf[64];
+    size_t rx_buf_size = 0;
+
+    while (rx_buf_size < sizeof(rx_buf))
+    {
+        // Put a chr in the buffer, its going to return 0 if successful
+        int ret = uart_poll_in(dev_demo_uart, &rx_buf[rx_buf_size]);
+
+        // No character ready yet!
+        if (ret != 0)
+            continue;
+
+        // Increment size
+        rx_buf_size++;
+
+        // End the loop when we reach a dot
+        if (rx_buf[rx_buf_size - 1] == '.')
+            break;
+    }
+
+    // Add null terminator at the end of the string
+    rx_buf[min(rx_buf_size, sizeof(rx_buf) - 1)] = 0;
+
+    // Check if the string was echo'd back to us
+    printk("UART Demo: Sent \"%s\", got back \"%s\"\n", msg, (char *)rx_buf);
+    __ASSERT(strcmp(rx_buf, msg) == 0, "UART demo mismatch");
+
+    printk("UART Demo: Done\n");
+}
 
 
 int main(void)
 {
+    // logging_init();
 
-
-    int ret;
-   // logging_init();
-
-    // int value = 0;
-
-    // ReSharper disable once CppDFAEndlessLoop
-    // while (1) {
-        // led_on_dt(&dev_led);
-        // ULOG_INFO("Blink number {}", value++);
-        // k_msleep(2000);
-    // }
-
-    while (false)
-    {
-        k_msleep(1000);
-
-        //i2c_configure_dt(&dev_test, I2C_SPEED_SET(I2C_SPEED_STANDARD) | I2C_MODE_CONTROLLER);
-
-        ret = i2c_is_ready_dt(&dev_test);
-        if (!ret) {
-            printk("Not ready yet!\n");
-            continue;
-        }
-
-
-        printk("Hi!\n");
-        ret = i2c_write_dt(&dev_test, (uint8_t[]){'A','C'}, 2);
-        printk("i2c_write_dt: %d\n", ret);
-        uint8_t buf[16];
-        ret = i2c_read_dt(&dev_test, buf, 2);
-        printk("i2c_read_dt: %d\n", ret);
-        printk("Buf is:\n");
-        for (int i=0; i<16; i++) {
-            printk("%02x ", buf[i]);
-            if ((i+1)%16 == 0) printk("\n");
-        }
-    }
+    demo_uart();
+    demo_i2c();
 
 }
