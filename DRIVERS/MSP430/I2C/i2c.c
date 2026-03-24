d59b0aa04e66c04ed49cbf8c547b9675a1744c90
Brendan Kelly <brewkelly@gmail.com>
2026-03-23 21:05:27 -0400

    msp i2c: nak writes when processing correctly
    alex's change UCTXNACK works different than I thought.

--- DRIVERS/MSP430/I2C/i2c.c
+++ DRIVERS/MSP430/I2C/i2c.c
@@ -18,7 +18,7 @@ void i2c_init_registers(uint8_t slave_addr)
     UCB0I2COA0 = slave_addr | UCOAEN;            // Own Address and enable
     UCB0CTLW0 &= ~UCSWRST;                       // clear reset register
 
-    UCB0IE |= UCSTPIE;                           // Enable STOP interrupt
+    UCB0IE |= UCSTTIE;                           // Enable START interrupt
     UCB0IE |= UCRXIE;                            // Enable RX interrupt
     UCB0IE |= UCTXIE;                            // Enable TX interrupt
 }
@@ -39,11 +39,18 @@ i2c_slave_state_t i2c_slave_current_state()
 void i2c_transition(i2c_slave_state_t state)
 {
     // naive for now
-    if (s_current_state == I2C_SLAVE_STATE_RESPONSE && state == I2C_SLAVE_STATE_REQUEST) {
+    if (state == I2C_SLAVE_STATE_REQUEST) {
         // discard tx buffer and reset 
         p_txbuf = NULL;
         s_tx_idx = 0;
         s_msg_len = 0;
+    }
+
+    // NACK if we are in processing stage
+    if (state == I2C_SLAVE_STATE_PROCESSING) {
+        UCB0CTLW0 |= UCTXNACK;
+    } else {
+        UCB0CTLW0 &= ~UCTXNACK;
     }
     
     s_current_state = state;
@@ -80,15 +87,17 @@ void __attribute__ ((interrupt(USCI_B0_VECTOR))) USCI_B0_ISR (void)
     case USCI_NONE:          break;         // Vector 0: No interrupts
     case USCI_I2C_UCALIFG:   break;         // Vector 2: ALIFG
     case USCI_I2C_UCNACKIFG: break;         // Vector 4: NACKIFG
-    case USCI_I2C_UCSTTIFG:  break;         // Vector 6: STTIFG
-    case USCI_I2C_UCSTPIFG:                 // Vector 8: STPIFG
-        // NOTE: Look into UCBxTBCNT, UCBCNTx, UCBCNTIFG for counting bytes
-        // Stop condition while master reading multiple times in a row ( e.g. header -> body)
-        if (s_tx_idx > 0 && s_tx_idx < s_msg_len) { 
-            // Roll back the last byte because we never got to send it
+    case USCI_I2C_UCSTTIFG:                 // Vector 6: STTIFG
+        if (s_current_state == I2C_SLAVE_STATE_PROCESSING) { 
+            UCB0CTLW0 |= UCTXNACK; // NACK master writes while we're processing
+        }
+
+        // Roll back 1 byte on start to transmit the byte that was missed during the last transmission
+        if (s_tx_idx > 0 && s_tx_idx <= s_msg_len) { 
             s_tx_idx--;
         }
         break;
+    case USCI_I2C_UCSTPIFG:  break;         // Vector 8: STPIFG
     case USCI_I2C_UCRXIFG3:  break;         // Vector 10: RXIFG3
     case USCI_I2C_UCTXIFG3:  break;         // Vector 12: TXIFG3
     case USCI_I2C_UCRXIFG2:  break;         // Vector 14: RXIFG2
@@ -96,19 +105,19 @@ void __attribute__ ((interrupt(USCI_B0_VECTOR))) USCI_B0_ISR (void)
     case USCI_I2C_UCRXIFG1:  break;         // Vector 18: RXIFG1
     case USCI_I2C_UCTXIFG1:  break;         // Vector 20: TXIFG1
     case USCI_I2C_UCRXIFG0:                 // Vector 22: RXIFG0  -> Receive one byte from MASTER (SAMV71)
-        if (s_current_state == I2C_SLAVE_STATE_PROCESSING) { 
-            (void)UCB0RXBUF; // clear the buffer
-            UCB0CTLW0 |= UCTXNACK; // NACK master writes while we're processing
-        } else {
-            s_ctx.i2c_rx_cb(UCB0RXBUF);
-        }
-        
+        s_ctx.i2c_rx_cb(UCB0RXBUF);
         break;
     case USCI_I2C_UCTXIFG0:                 // Vector 24: TXIFG0  -> Send one byte to MASTER (SAMV71)
-        // if we are asked for more bytes than we need, or we're currently processing send 0xFF
-        if (s_tx_idx >= s_msg_len || s_current_state == I2C_SLAVE_STATE_PROCESSING) {
+        if (s_current_state != I2C_SLAVE_STATE_RESPONSE) { // Unless we are in response state, just return 0xFF
+            UCB0TXBUF = 0xFF;
+        } else if (!p_txbuf || s_tx_idx >= s_msg_len) { // if we are asked for more bytes than we have, send 0xFF
+            // If we are at the byte after the last byte, go one over to make sure we dont roll back on start 
+            // (by now the controller has received atleast one 0xFF)
+            if (s_tx_idx == s_msg_len) {
+                s_tx_idx++;
+            }
             UCB0TXBUF = 0xFF; 
-        } else if (p_txbuf && s_current_state == I2C_SLAVE_STATE_RESPONSE) {
+        } else {
             UCB0TXBUF = p_txbuf[s_tx_idx++]; 
         }
         break;
